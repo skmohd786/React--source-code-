f35bfb006376ca247a06f6404f9d553be34b8db5
shaikh mohd <shaikhmohd00009@gmail.com>
2026-07-27 04:20:12 +0530

    usestate

---
+++ 08-funtions/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const App = () => {
+
+  const [bgcolor,setbgcolor] = useState("bg-amber-950");   // use state const [read,write] = useState(intial value of read)
+
+  const changecolor=  ()=>{
+    if(bgcolor == "bg-amber-950") setbgcolor("bg-white-300");
+    else setbgcolor("bg-amber-950");
+  };
+
+  return (
+    <div className={`flex h-screen justify-center items-center ${bgcolor}`}>
+      <button onClick={changecolor}> <h1 className="bg-blue-200 w-40 h-15 flex justify-center items-center rounded-2xl">hello its Niet</h1>
+      </button>
+    </div>
+  )
+}
+
+
+export default App
