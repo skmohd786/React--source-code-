d85e6fffab7af1cd6bb2f8e8e351ccf9b2cb714a
shaikh mohd <shaikhmohd00009@gmail.com>
2026-07-28 08:33:44 +0530

    two way binding

--- 09-form-handling/src/App.jsx
+++ 09-form-handling/src/App.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 
 const App = () => {
+
+  const [name,setname] = useState('shaikh');
 
   const submithandler = (e) =>{
     e.preventDefault();
@@ -12,7 +15,13 @@ const App = () => {
       <form className=' h-1/2 flex justify-center items-center' onSubmit={(e)=>{
         submithandler(e);
       }}>
-        <input type="text" className='border-[2px] border-black text-white' placeholder='Enter the text'/>
+        <input type="text" className='border-[2px] border-black text-white' placeholder='Enter the text'
+         value={name}
+         onChange={(e)=>{
+          setname(e.target.value)
+         }}
+         
+         />
         <button  className='ml-5 text-lg text-blue font-bold px-4 bg-purple-500 rounded-full ' >Submit</button>
       </form>
     </div>
